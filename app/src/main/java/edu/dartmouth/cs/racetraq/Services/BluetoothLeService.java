b0c8c8c9852a57a75923c8f509e616f9d8048394
Madeleine McDermott <madeleine.c.mcdermott.19@dartmouth.edu>
2019-05-22 04:03:06 -0400

    ble connection working

---
+++ app/src/main/java/edu/dartmouth/cs/racetraq/Services/BluetoothLeService.java
@@ -0,0 +1,171 @@
+package edu.dartmouth.cs.racetraq.Services;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.UUID;
+
+import edu.dartmouth.cs.racetraq.ConnectActivity;
+
+public class BluetoothLeService extends Service {
+
+    private final static String TAG = BluetoothLeService.class.getSimpleName();
+
+    private static boolean isRunning = false;
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter bluetoothAdapter;
+    private String bluetoothDeviceAddress;
+    private BluetoothDevice bluetoothDevice;
+    private BluetoothGatt bluetoothGatt;
+    private int connectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    public final static String ACTION_GATT_CONNECTED = "edu.dartmouth.cs.racetraq.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED = "edu.dartmouth.cs.racetraq.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED = "edu.dartmouth.cs.racetraq.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE = "edu.dartmouth.cs.racetraq.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA = "edu.dartmouth.cs.racetraq.EXTRA_DATA";
+
+
+    /**
+     * OVERRIDE METHODS
+     **/
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        isRunning = true;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // Set up bluetooth
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Connect to device
+        bluetoothDeviceAddress = intent.getStringExtra(ConnectActivity.DEVICE_ADDRESS_KEY);
+        bluetoothDevice = bluetoothAdapter.getRemoteDevice(bluetoothDeviceAddress);
+        if (bluetoothDevice == null) {
+            Log.w(TAG, "Device not found.  Unable to connect.");
+
+            // broadcast connect failed
+        }
+        else
+        {
+            bluetoothGatt = bluetoothDevice.connectGatt(getApplicationContext(), false, gattCallback);
+        }
+
+        return START_STICKY;
+    }
+
+    /**
+     * STATIC METHODS
+     */
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
+
+
+    /**
+     * PRIVATE METHODS
+     */
+    // Various callback methods defined by the BLE API.
+    private final BluetoothGattCallback gattCallback =
+            new BluetoothGattCallback() {
+                @Override
+                public void onConnectionStateChange(BluetoothGatt gatt, int status,
+                                                    int newState) {
+                    String intentAction;
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        intentAction = ACTION_GATT_CONNECTED;
+                        connectionState = STATE_CONNECTED;
+                        broadcastUpdate(intentAction);
+                        Log.i(TAG, "Connected to GATT server.");
+                        Log.i(TAG, "Attempting to start service discovery:" +
+                                bluetoothGatt.discoverServices());
+
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        intentAction = ACTION_GATT_DISCONNECTED;
+                        connectionState = STATE_DISCONNECTED;
+                        Log.i(TAG, "Disconnected from GATT server.");
+                        broadcastUpdate(intentAction);
+                    }
+                }
+
+                @Override
+                // New services discovered
+                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+                    } else {
+                        Log.w(TAG, "onServicesDiscovered received: " + status);
+                    }
+                }
+
+                @Override
+                // Result of a characteristic read operation
+                public void onCharacteristicRead(BluetoothGatt gatt,
+                                                 BluetoothGattCharacteristic characteristic,
+                                                 int status) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+                    }
+                }
+
+            };
+
+    private void broadcastUpdate(final String action) {
+        final Intent intent = new Intent(action);
+        sendBroadcast(intent);
+    }
+
+    private void broadcastUpdate(final String action, final BluetoothGattCharacteristic characteristic) {
+        final Intent intent = new Intent(action);
+
+        // This is special handling for the Heart Rate Measurement profile. Data
+        // parsing is carried out as per profile specifications.
+        // For all other profiles, writes the data formatted in HEX.
+        final byte[] data = characteristic.getValue();
+        if (data != null && data.length > 0) {
+            final StringBuilder stringBuilder = new StringBuilder(data.length);
+            for (byte byteChar : data)
+                stringBuilder.append(String.format("%02X ", byteChar));
+            intent.putExtra(EXTRA_DATA, new String(data) + "\n" +
+                    stringBuilder.toString());
+        }
+        sendBroadcast(intent);
+    }
+
+    /**
+     * After using a given BLE device, the app must call this method to ensure resources are  released properly.
+     */
+    private void close() {
+        if (bluetoothGatt != null) {
+            bluetoothGatt.close();
+            bluetoothGatt = null;
+            bluetoothDeviceAddress = null;
+            bluetoothDevice = null;
+        }
+    }
+
+}
