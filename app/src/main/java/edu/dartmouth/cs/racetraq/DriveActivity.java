8877ce1c4d80e93f8380c598711105f0fb8674bc
Alexander Newman <alexander.s.newman.19@dartmouth.edu>
2019-05-25 16:30:23 -0400

    driveActivity update

--- app/src/main/java/edu/dartmouth/cs/racetraq/DriveActivity.java
+++ app/src/main/java/edu/dartmouth/cs/racetraq/DriveActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.location.Location;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.IBinder;
